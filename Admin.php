2672d90d41d11099fd02f8280ffb60529686b4a2
NicolasD1440 <nicolascastiblanco6@gmail.com>
2021-11-14 16:21:00 -0500

    Cambios --- no trabajar sobre la rama principal

--- Admin.php
+++ Admin.php
@@ -1,3 +1,4 @@
+
 <html lang="en">
 <head>
   <meta charset="UTF-8"> <!--Para aceptar caracteres especiales-->
@@ -11,11 +12,11 @@
 
 </head>
 <body>
-    <div class="Administrador col-5"> <!--contenedor de la decicion-->
+    <div class="Administrador col-10 col-sm-5"> <!--contenedor de la decicion-->
       <img src="img/LOGO.png" class="img-fluid" alt="Logo del jardin">
-      <form class="Form_Datos col-11 text-center" method="post" action="Tabla_citas.php">
-        <input type="text" id="Admin" placeholder="Administrador" class="Datos col-10" required>
-        <input type="password" id="Clave" placeholder="Contraseña" class="Datos col-10" required>
+      <form class="Form_Datos col-11 text-center"  method="post">
+        <input type="text" id="Admin" name="usu" placeholder="Administrador" class="Datos col-10" required>
+        <input type="password" id="Clave" name="cont" placeholder="Contraseña" class="Datos col-10" required>
         <input type="submit" name="Ingresar" value="Ingresar" class="Asignacion col-5">
         <button type="button" class="Asignacion col-5" onclick="location.href='Decision.html'">Volver</button>
       </form>
@@ -23,3 +24,44 @@
   <footer>© Creado por Cristian Giovani Cruz Herrera Deivy Nicolas Castiblanco Infante & Johan Daniel Chavez Celeita</footer>
 </body>
 </html>
+<?php
+
+session_start();
+
+if (isset($_POST['Ingresar'])) {
+include('Conexion.php');
+$usuario=$_POST['usu'];
+$contraseña=$_POST['cont'];
+
+
+$_SESSION['Admin']=$usuario;
+
+
+$conexion=mysqli_connect("localhost","root","","jardin");
+
+$consulta="SELECT*FROM admin where Admin ='$usuario' and Contraseña='$contraseña'";
+$resultado=mysqli_query($conexion,$consulta);
+
+$filas=mysqli_num_rows($resultado);
+
+if($filas){
+
+    echo"<script>
+alert('BIENVENIDO ADMIN');
+location.href='Tabla_citas.php'
+</script>";
+
+
+}else{
+    ?>
+    <?php
+   echo"<script>
+alert('ERROR DE AUTENTIFICACION');
+location.href='Admin.php'
+</script>";
+
+
+}
+}
+mysqli_free_result($resultado);
+mysqli_close($conexion);
